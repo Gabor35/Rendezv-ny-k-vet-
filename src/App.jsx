efd5f24b111777e1ec1b28d0ab821e50d296634f
Gabor35 <pusztaig@kkszki.hu>
2025-02-10 09:28:15 +0100

    kommentek

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { AddEvent } from './AddEvent';
 import { EventList } from './EventList';
 import logo from './logo.jpg';
 
-//kommentek
+//komment
 export const App = () => {
   return (
     <Router>
