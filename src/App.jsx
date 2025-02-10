3b7463868eedf05e971b38ab0b75762a9e9635a0
Gabor35 <pusztaig@kkszki.hu>
2025-02-10 09:40:05 +0100

    xd

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
-import { Login } from './Login';
+import { Login } from './Login'; 
 import { Register } from './Register';
 import { ForgotPassword } from './ForgotPassword';
 import { AddEvent } from './AddEvent';
